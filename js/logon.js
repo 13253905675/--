ae0433d7418dc9009a92a8b72d2ce9baa1663a6a
Xi <2163785031@qq.com>
2023-03-30 21:59:05 +0800

    第二次提交

--- js/logon.js
+++ js/logon.js
@@ -3,10 +3,26 @@ var logon = document.getElementsByClassName("logon")[0];
 // var email=document.getElementById('userEmail').value;
 
 logon.onclick = function () {
-   
-            alert('注册成功，欢迎进入');
-            window.location.href = '../html/behind.html';
+    var email = document.getElementById('userEmail').value;
+
+    if (email == "") {
+        alert("邮箱为空，请重新输入！");
+    }
+    if (email != "") {
+        var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        isok = reg.test(email);
+        if (!isok) {
+            alert("邮箱格式不正确，请重新输入！");
+
         }
+    
+    else if(isok){ 
+            alert('注册成功!');
+            window.location.href = '../html/login.html';
+
+        }
+    }
+}
     
 
 var text = document.getElementsByClassName("text")[0]; //获取验证码按钮
